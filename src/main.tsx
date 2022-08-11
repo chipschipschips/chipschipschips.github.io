4f564b7bcffc55e8187a86c53e6de32b71598ace
Chris <chris@rarr.net>
2022-08-11 09:22:39 -0700

    Create a basic react-based page

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize-css';
+
+ReactDOM
+  .createRoot(document.getElementById('root') as HTMLElement)
+  .render(
+    <React.StrictMode>
+      <h1>Hello, world!</h1>
+    </React.StrictMode>
+  );
