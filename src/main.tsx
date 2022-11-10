aea324cbd438b8ea5a9e8b8385a8a9506211d5fb
Chris Dowling <chris@rarr.net>
2022-11-10 10:34:37 -0800

    Use StackBlitz svg from @icons-pack/react-simple-icons
    fixes #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Codepen, Github, Jsfiddle} from '@icons-pack/react-simple-icons';
+import {
+  Codepen,
+  Github,
+  Jsfiddle,
+  Stackblitz
+} from '@icons-pack/react-simple-icons';
 import 'normalize-css';
 import './page.css';
 
@@ -20,16 +25,7 @@ ReactDOM
           </li>
           <li key={2}>
             <a href="https://stackblitz.com/@chipschipschips">
-              {/* TODO(issues/1): Get from icons-pack once supported */}
-              <svg viewBox="0 0 24 24"
-                   fill="grey"
-                   height="1.5em"
-                   width="1.5em"
-              >
-                <title>stackblitz</title>
-                <path
-                  d="M12.747 16.273h-7.46L18.925 1.5l-3.671 10.227h7.46L9.075 26.5l3.671-10.227z"/>
-              </svg>
+              <Stackblitz title="StackBlitz" size={"1.5em"} color="grey"/>
             </a>
           </li>
           <li key={3}>
